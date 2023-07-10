380d611c6a29dff2aeb3efffc870ef871bc15408
vikasavchenko <viktorichernobaj@gmail.com>
2023-07-10 04:07:35 +0300

    mobile

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,24 @@
 (() => {
     const refs = {
+      modal: document.querySelector("[data-modal]"),
       openModalBtn: document.querySelector("[data-modal-open]"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
+      hamburgerMenu: document.querySelector("[data-modal-hamburger-menu]"),
+      openHamburgerMenuBtn: document.querySelector("[data-modal-hamburger-menu-open]"),
+      closeHamburgerMenuBtn: document.querySelector("[data-modal-hamburger-menu-close]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    refs.openHamburgerMenuBtn.addEventListener("click", toggleModalHamburgerMenu);
+    refs.closeHamburgerMenuBtn.addEventListener("click", toggleModalHamburgerMenu);
   
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
+    }
+
+    function toggleModalHamburgerMenu() {
+      refs.hamburgerMenu.classList.toggle("is-hidden");
     }
   })();
